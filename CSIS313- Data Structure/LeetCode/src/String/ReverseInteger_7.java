7511fb9526b8790cfee10eb2bbaa61aef6316761
Shuhuasong <shuhuasong888@gmail.com>
2020-01-14 00:08:36 -0500

    Tree Topic Easy Part

---
+++ CSIS313- Data Structure/LeetCode/src/String/ReverseInteger_7.java
@@ -0,0 +1,17 @@
+package String;
+
+public class ReverseInteger_7 {
+    public int reverse(int x) {
+        int rev = 0;
+        while(x!=0){
+            int pop = x % 10;
+            x /= 10;
+            if(rev > Integer.MAX_VALUE/10 || rev ==Integer.MAX_VALUE/10 && pop > 7) return 0;
+            if(rev < Integer.MIN_VALUE/10 || rev ==Integer.MIN_VALUE/10 && pop < -8) return 0;
+            //Integer.MAX_VALUE=2147483647
+            //make sure that pop is not > 7 (2147483647 - 214748364 * 10)
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
