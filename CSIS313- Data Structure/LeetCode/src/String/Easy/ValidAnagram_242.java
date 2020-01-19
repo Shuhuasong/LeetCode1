a899a3ae74c3875969504c2bb4a96c3a43c506a0
Shuhuasong <shuhuasong888@gmail.com>
2020-01-18 23:55:55 -0500

    Tree Topic Easy Part

--- CSIS313- Data Structure/LeetCode/src/String/Easy/ValidAnagram_242.java
+++ CSIS313- Data Structure/LeetCode/src/String/Easy/ValidAnagram_242.java
@@ -1,21 +1,23 @@
 package String.Easy;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ValidAnagram_242 {
 
     //Time: O(n)
     //Space: O(1)
+
+    //run: 4ms (80%)
     public static boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
 
         int[] ch = new int[26];
         for(int i=0; i<s.length(); i++){
             ch[s.charAt(i)-'a']++;
-        }
-        for(int i=0; i<s.length(); i++){
             ch[t.charAt(i)-'a']--;
         }
+
         for(int i : ch)
             if(i != 0) return false;
         return true;
@@ -31,6 +33,28 @@ public class ValidAnagram_242 {
         b = "cx";
         System.out.println(isAnagram(a, b));
     }
+
+//Time: O(nlog n) Space: O(n)
+//Run: 6ms (49%)
+  /*  public boolean isAnagram(String s, String t) {
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+        Arrays.sort(s1); //O(nlog n)
+        Arrays.sort(t1);
+
+        int n = s1.length;
+        int m = t1.length;
+        if(n!=m)
+            return false;
+        else{
+            for(int i=0; i<s1.length; i++){
+                if(s1[i] != t1[i]) return false;
+            }
+        }
+        return true;
+    }
+
+   */
 
   /*  public boolean isAnagram(String s, String t) {
         if(s==null || t==null)  return false;
---
+++ CSIS313- Data Structure/LeetCode/src/String/Medium/GroupShiftedString_249.java
@@ -0,0 +1,24 @@
+package String.Medium;
+
+/*
+Given a string, we can "shift" each of its letter to its successive letter, for example: "abc" -> "bcd". We can keep "shifting" which forms the sequence: "abc" -> "bcd" -> ... -> "xyz".
+
+Given a list of strings which contains only lowercase alphabets, group all strings that belong to the same shifting sequence, return:
+Eg: given: ["abc","bcd","acef", "xyz","az","ba","a","z"]
+[
+  ["abc","bcd","xyz"],
+  ["az","ba"],
+  ["acef"],
+  ["a","z"]
+]
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupShiftedString_249 {
+    public List<List<String>> groupStrings(String[] strings){
+        List<List<String>> res = new ArrayList<>();
+    }
+
+}
